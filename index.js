2f5f7d598d986054baef5c8c31f4bda049a1b76b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-12 20:20:10 +0600

    completed the database setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,11 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send('Visualization dashboard server is running');
