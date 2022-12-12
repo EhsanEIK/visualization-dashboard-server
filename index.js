5dfa83198bbad2dbbeaa16ca7c0a1e93cbbcb546
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-12 20:24:34 +0600

    added data GET api to get the data from db

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.use(express());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const dataCollection = client.db('visulaizationDashboardDB').collection('dataCollection');
+
+        app.get('/data', async (req, res) => {
+            const query = {};
+            const data = await dataCollection.find(query).toArray();
+            res.send(data);
+        })
+    }
+    finally { }
+}
+
+run().catch(err => console.error(err));
+
 
 app.get('/', (req, res) => {
     res.send('Visualization dashboard server is running');
